42c765f88d221de647eeac7087f8ace47c37ace3
itorr <itorr@unc.edu>
2024-08-22 07:40:05 -0400

    Finished exercise 00

---
+++ lessons/clo1.py
@@ -0,0 +1,9 @@
+# objects types of units. ex. 1, 3.2
+# type classifies objects to help computer know how to interpret it ex. numerical,
+# textual, sequencs,
+# groupings of different types ex. type("1234"[0]) = class 'str'
+# int : integers ex. 0, 12,
+# float : decimals ex. 0.243
+# str : sequence or string of characters ex. "Howdy"
+# indexing allows you to access an item in a sequence; index numbering starts at 0
+# ex. "Howdy"[0] = 'H'
